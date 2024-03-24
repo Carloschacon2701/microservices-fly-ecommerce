6bd250ddad5a20da07becb73a6ec17b544e6f999
Carlos <carlosdchaconmendez@gmail.com>
2024-03-24 11:14:02 -0400

    Feature: Fly core added

---
+++ fly-core/src/test/java/org/carlos/flycore/FlyCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carlos.flycore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
