900d13f4b80a7bfe9893ce1b7a871fb5b9cbb92b
Carlos <carlosdchaconmendez@gmail.com>
2024-03-24 11:37:13 -0400

    Fix

---
+++ fly_core/src/main/java/org/carlos/fly_core/FlyCoreApplication.java
@@ -0,0 +1,13 @@
+package org.carlos.fly_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyCoreApplication.class, args);
+	}
+
+}
---
+++ fly_core/src/test/java/org/carlos/fly_core/FlyCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carlos.fly_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
